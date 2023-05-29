01b19fce52f0566b9f2f79fb0ee34a0cd511b047
안동환 <@LAPTOP-PJ9GIV42>
2023-05-29 18:59:46 +0900

    week12 HW

---
+++ solveRangeInPerifocalFrame.m
@@ -0,0 +1,9 @@
+%%항공우주공학과 20011321 안채원
+%%우주궤도역학 week#12 HW
+
+function rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomoly)
+
+r = semimajor_axis*(1-eccentricity^2) / (1+eccentricity*cos(true_anomoly));
+rangeInPQW = [r*cos(true_anomoly) ; r*sin(true_anomoly) ; 0];
+
+end
